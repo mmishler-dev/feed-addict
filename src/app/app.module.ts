07ca24f118a1dd87f9013b3c70ea7cde868d6f16
mmishler-dev <matthewvmishler@gmail.com>
2019-06-14 13:54:59 -0700

    Initial route and project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { MyAccountComponent } from './components/my-account/my-account.component';
+import { SettingsComponent } from './components/my-account/settings/settings.component';
+import { AboutComponent } from './components/about/about.component';
+import { TermsAndConditionsComponent } from './components/terms-and-conditions/terms-and-conditions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    FeedComponent,
+    LoginComponent,
+    SignupComponent,
+    MyAccountComponent,
+    SettingsComponent,
+    AboutComponent,
+    TermsAndConditionsComponent
   ],
   imports: [
     BrowserModule,
